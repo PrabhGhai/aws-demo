e98814b5893f4077f7ada184fa42e1f9f9a32cbb
Prabhjot Singh <ghaiprabhghai@gmail.com>
2025-07-03 22:48:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello First time docker");
+});
+app.listen(1000, () => {
+  console.log("Server Started");
+});
