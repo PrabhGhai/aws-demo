d3ba8d1f7c47a7ec07a0d68c2a52b4c7bbf91e05
Prabhjot Singh <101247146+PrabhGhai@users.noreply.github.com>
2025-07-04 21:26:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello First time docker");
 });
-app.listen(1000, () => {
+
+app.listen(1000, '0.0.0.0', () => {
   console.log("Server Started");
 });
